ed5a52ea75cab542ae814bb744406f12186c059f
Mo-Ibra <mohamedelgedwy245@gmail.com>
2025-02-16 02:27:54 +0200

    Added Spinner And Fix issues

--- sections/PopularCars.jsx
+++ sections/PopularCars.jsx
@@ -5,8 +5,19 @@ import Link from "next/link"
 import { popularCars } from "@/data/cars"
 
 import CarCard from "@/components/CarCard"
+import { Loader2 } from "lucide-react"
+import { useEffect, useState } from "react"
 
 function PopularCars() {
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }, [])
+
     return (
         <section className="bg-gray-100">
             <div className="container mx-auto px-4 py-8">
@@ -16,11 +27,17 @@ function PopularCars() {
                         View All
                     </Link>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                    {popularCars.map((car) => (
-                        <CarCard key={car.id} car={car} />
-                    ))}
-                </div>
+                {isLoading ? (
+                    <div className="flex items-center justify-center h-full">
+                        <Loader2 className="h-10 w-10 animate-spin" />
+                    </div>
+                ) : (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                        {popularCars.map((car) => (
+                            <CarCard key={car.id} car={car} />
+                        ))}
+                    </div>
+                )}
             </div>
         </section>
     )
--- sections/RecomendationCars.jsx
+++ sections/RecomendationCars.jsx
@@ -5,8 +5,19 @@ import Link from "next/link"
 import { recommendationCars } from "@/data/cars"
 
 import CarCard from "@/components/CarCard"
+import { useEffect, useState } from "react"
+import { Loader2 } from "lucide-react"
 
 function RecomendationCars({ withButton = true, carLimit}) {
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }, [])
+
     return (
         <section className="bg-gray-100">
             <div className="container mx-auto px-4 py-8">
@@ -16,12 +27,18 @@ function RecomendationCars({ withButton = true, carLimit}) {
                         View All
                     </Link>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                    {recommendationCars.slice(0, carLimit).map((car) => (
-                        <CarCard key={car.id} car={car} />
-                    ))}
-                </div>
-                {withButton && (
+                {isLoading ? (
+                    <div className="flex items-center justify-center h-full">
+                        <Loader2 className="h-10 w-10 animate-spin" />
+                    </div>
+                ) : (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                        {recommendationCars.slice(0, carLimit).map((car) => (
+                            <CarCard key={car.id} car={car} />
+                        ))}
+                    </div>
+                )}
+                {!isLoading && withButton && (
                     // See more cars button
                     <div className="flex justify-center my-16">
                         <Link href="/category" className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors">See More Cars</Link>
